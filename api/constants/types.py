e4c481caae02b6376f3360ed666744c340d5c93c
조한슬(Hanseul Jo) <bryantjo1224@gmail.com>
2023-05-11 17:29:54 +0900

    허용 파일 확장자 검사 기능 추가에 따른 상수 추가

--- api/constants/types.py
+++ api/constants/types.py
@@ -2,6 +2,30 @@ from api.constants.base import BaseEnum
 
 
 class FileTypesEnum(str, BaseEnum):
-    images: str = "image"
-    text: str = "text"
-    video: str = "video"
+    IMAGE: str = "image"
+    DOCUMENT: str = "document"
+    VIDEO: str = "video"
+
+
+class ImageFileTypesEnum(str, BaseEnum):
+    JPG: str = "jpg"
+    JPEG: str = "jpeg"
+    PNG: str = "png"
+    GIF: str = "gif"
+    BMP: str = "bmp"
+
+
+class DocumentFileTypesEnum(str, BaseEnum):
+    TXT: str = "txt"
+    DOC: str = "doc"
+    DOCX: str = "docx"
+    PPT: str = "ppt"
+    PPTX: str = "pptx"
+    PDF: str = "PDF"
+
+
+class VideoFileTypesEnum(str, BaseEnum):
+    MP4: str = "mp4"
+    AVI: str = "avi"
+    MOV: str = "mov"
+    WMV: str = "wmv"
