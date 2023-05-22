25bcbc87d09dbd2e1a021c5f281f2e305f348374
조한슬(Hanseul Jo) <bryantjo1224@gmail.com>
2023-05-22 14:02:33 +0900

    Refactor: FileService 메서드 분리

--- api/services/samples/file.py
+++ api/services/samples/file.py
@@ -22,27 +22,39 @@ from api.repos.sample import (
 
 class SampleFileService:
     def upload(self, request: HttpRequest):
-        """
-        Client 로 부터 전달 받은 request로 부터 File을 Upload
-        """
+        """ Client 로 부터 전달 받은 request로 부터 File을 Upload """
         files = request.FILES.getlist(key='file', default=None)
         serializers = self._get_file_serializers(files=files)
         self._save(serializers=serializers)
 
+    def update(self, request: HttpRequest, pk: int):
+        instance = sample_file_repo.get(pk=pk)
+        file = request.FILES.get(key='file', default=None)
+        serializer = self._create_sample_file_serializer(file=file, instance=instance)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data
+        else:
+            print("Error!", traceback.format_exc())
+
     def _get_file_serializers(self, *, files: QueryDict) -> List[SampleFileSerializer]:
-        """
-        파일 개수(단, 복수) 와 관계 없이 Serializers 생성
-        """
-        serializers: List[SampleFileSerializer] = [
-            SampleFileSerializer(data=FileInputData(
+        """ 파일 개수(단, 복수) 와 관계 없이 Serializers 생성 """
+        serializers = [
+            self._create_sample_file_serializer(file=file)
+            for file in files
+        ]
+        return serializers
+
+    def _create_sample_file_serializer(self, *, file, instance=None) -> SampleFileSerializer:
+        return SampleFileSerializer(instance, data=self._create_file_input_data(file=file))
+
+    def _create_file_input_data(self, *, file):
+        return FileInputData(
                 file=file.name,
                 file_path=file,
                 extension=get_file_extension(file.name),
                 type=get_file_type(file.name),
-            ).__dict__)
-            for file in files]
-
-        return serializers
+            ).__dict__
 
     @transaction.atomic
     def _save(self, *, serializers: List[SampleFileSerializer]):
@@ -53,6 +65,10 @@ class SampleFileService:
                 print(traceback.format_exc())
                 raise ValueError("파일 업로드 실패!")
 
+    def delete(self, pk: int):
+        instance = sample_file_repo.get(pk=pk)
+        instance.delete()
+
     def get_multi(self):
         instance = sample_file_repo.get_multi()
         serializer = SampleFileSerializer(instance, many=True)
@@ -62,25 +78,6 @@ class SampleFileService:
         data = sample_file_repo.get(pk=pk)
         serializer = SampleFileSerializer(data)
         return serializer.data
-
-    def update(self, request: HttpRequest, pk: int):
-        instance = sample_file_repo.get(pk=pk)
-        file = request.FILES.get(key='file', default=None)
-        serializer = SampleFileSerializer(instance, data=FileInputData(
-                file=file.name,
-                file_path=file,
-                extension=get_file_extension(file.name),
-                type=get_file_type(file.name),
-            ).__dict__)
-        if serializer.is_valid():
-            serializer.save()
-            return serializer.data
-        else:
-            print("Error!", traceback.format_exc())
-
-    def delete(self, pk: int):
-        instance = sample_file_repo.get(pk=pk)
-        instance.delete()
 
 
 @dataclass
